acada8808e33bc610b533b917b3382e8602842a1
Jai <jazzyb2006@gmail.com>
2018-07-18 10:36:11 +0530

    initial commit

---
+++ src/test/java/test.java
@@ -0,0 +1,32 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+public class test {
+
+    String driverPath = "<path to gecko driver executable>";
+    public WebDriver driver;
+
+    @Test
+    public void launchBrowser() {
+        System.out.println("launching firefox browser");
+        //System.setProperty("webdriver.chrome.driver", "./Resources/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "./Resources/geckodriver.exe");
+
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void openApplication() {
+        driver.navigate().to("http://www.google.com");
+    }
+
+    @Test
+    public void closeDriver() {
+        if(driver!=null) {
+            driver.close();
+        }
+    }
+
+}
